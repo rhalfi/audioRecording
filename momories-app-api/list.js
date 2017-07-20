3e27f3d0a7954e53642c73dd57561257269f9bbf
Ram Halfi <ramh@fring.com>
2017-07-20 16:00:27 +0300

    first commit

---
+++ momories-app-api/list.js
@@ -0,0 +1,29 @@
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: 'moments',
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId' partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be User Pool sub of the authenticated user
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.authorizer.claims.sub,
+    }
+  };
+
+  try {
+    var result = await dynamoDbLib.call('query', params);
+//console.log(result.Items);
+   result.Items.sort(function(a, b) {
+    return parseInt(b.createdAt) - parseInt(a.createdAt);
+});
+    // Return the matching list of items in response body
+    callback(null, success(result.Items));
+  }
+  catch(e) {
+    callback(null, failure({status: false}));
+  }
+};
